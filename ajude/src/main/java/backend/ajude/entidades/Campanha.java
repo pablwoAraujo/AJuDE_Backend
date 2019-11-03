e91e9f898418cd9ac4b740aa1b99a49744b9f9fe
Luiz Fernando <you@example.com>
2019-11-03 14:13:46 -0300

    doizada

---
+++ ajude/src/main/java/backend/ajude/entidades/Campanha.java
@@ -0,0 +1,128 @@
+package backend.ajude.entidades;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Campanha {
+    @Id
+    @GeneratedValue
+    private long id;
+    private String nome;
+    private String URL;
+    private String descricao;
+    private String data;
+    private String status;
+    private double meta;
+    private double doacao;
+    private Usuario dono;
+    private String comentarios;
+    private int likes;
+
+    public Campanha() {
+        super();
+    }
+
+    public Campanha(long id, String nome, String URL, String descricao, String data, String status, double meta, double doacao, Usuario dono, String comentarios, int likes) {
+        this.id = id;
+        this.nome = nome;
+        this.URL = URL;
+        this.descricao = descricao;
+        this.data = data;
+        this.status = status;
+        this.meta = meta;
+        this.doacao = doacao;
+        this.dono = dono;
+        this.comentarios = comentarios;
+        this.likes = likes;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getURL() {
+        return URL;
+    }
+
+    public void setURL(String URL) {
+        this.URL = URL;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public double getMeta() {
+        return meta;
+    }
+
+    public void setMeta(double meta) {
+        this.meta = meta;
+    }
+
+    public double getDoacao() {
+        return doacao;
+    }
+
+    public void setDoacao(double doacao) {
+        this.doacao = doacao;
+    }
+
+    public Usuario getDono() {
+        return dono;
+    }
+
+    public void setDono(Usuario dono) {
+        this.dono = dono;
+    }
+
+    public String getComentarios() {
+        return comentarios;
+    }
+
+    public void setComentarios(String comentarios) {
+        this.comentarios = comentarios;
+    }
+
+    public int getLikes() {
+        return likes;
+    }
+
+    public void setLikes(int likes) {
+        this.likes = likes;
+    }
+}
---
+++ ajude/src/main/java/backend/ajude/repositories/CampanhasRepository.java
@@ -0,0 +1,16 @@
+package backend.ajude.repositories;
+
+import java.io.Serializable;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import backend.ajude.entidades.Campanha;
+
+@Repository
+public interface CampanhasRepository<T, ID extends Serializable> extends JpaRepository<Campanha, String> {
+
+    //@Query()
+    Optional<Campanha> findByNome(String nome);
+}
