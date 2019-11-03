bb65bdf4bb8e06f6e3f8650246a507b651efa9a6
Luiz Fernando <you@example.com>
2019-11-03 15:06:40 -0300

    dois

--- ajude/src/main/java/backend/ajude/servicos/CampanhasService.java
+++ ajude/src/main/java/backend/ajude/servicos/CampanhasService.java
@@ -4,11 +4,29 @@ import java.util.Optional;
 
 import javax.servlet.ServletException;
 
+import backend.ajude.entidades.Campanha;
 import org.springframework.stereotype.Service;
 
-import backend.ajude.entidades.Usuario;
+import backend.ajude.entidades.Campanha;
 import backend.ajude.repositories.UsuariosRepository;
 
 public class CampanhasService {
-    private UsuariosRepository<Usuario, String> usuariosDAO;
+    private UsuariosRepository<Campanha, Long> campanhasDAO;
+
+    public CampanhasService(UsuariosRepository<Campanha, Long> campanhasDAO){
+        super();
+        this.campanhasDAO = campanhasDAO;
+    }
+
+    public Campanha adicionaCampanha(Campanha campanha) throws ServletException {
+        Optional<Campanha> provisory = campanhasDAO.findById(campanha.getId());
+        if(provisory.isPresent()) {
+            throw new ServletException("Campanha já Cadastrada");
+        }
+        return campanhasDAO.save(campanha);
+    }
+
+    public
+
+
 }
